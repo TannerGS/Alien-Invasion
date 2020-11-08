f9980232e087d24ff923bf8fffe6f685fb8945d2
tanner <tannersimpkins@gmail.com>
2020-11-08 13:48:36 -0600

    Added horizontal movement with screen limit. Added speed limit.

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+from ship import Ship
+
 class Settings:
 	"""A class to store all settings for Alien Invasion"""
 
@@ -6,5 +8,8 @@ class Settings:
 		self.screen_width = 1200
 		self.screen_height = 800
 		self.bg_color = (230, 230, 230)
+		self.ship_speed = 1.5
+	
+
 
 	
