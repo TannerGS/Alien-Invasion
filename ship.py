ed0e93304ac3dd80fc5b116b581cffe2d607d131
tanner <tannersimpkins@gmail.com>
2020-11-09 20:17:04 -0600

    Added game stats, modified settings, added collision detection, and added  moving aliens.

--- ship.py
+++ ship.py
@@ -27,6 +27,11 @@ class Ship:
 		"""Draw the ship at its current location."""
 		self.screen.blit(self.image, self.rect)
 
+	def center_ship(self):
+		"""Center the ship on the screen."""
+		self.rect.midbottom = self.screen_rect.midbottom
+		self.x = float(self.rect.x)
+
 	def update(self):
 		"""Update the ships positon based on movement flags"""
 		# Update the ship's x value, not the rect.
@@ -37,3 +42,4 @@ class Ship:
 	
 		# Update rect object from self.x.
 		self.rect.x = self.x
+	
